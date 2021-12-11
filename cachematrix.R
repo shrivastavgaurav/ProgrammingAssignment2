0b63677fac6906cec7efefea0f437408d5b87d89
Gaurav Shrivastav <gauravshrivastav7@gmail.com>
2021-12-11 15:42:05 +0530

    Comments in the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,9 @@
-
+## The cachematrix.R function has a matrix argument and builds
+## a set of functions and returns them to a list in the parent environment
+## A sample matrix: m1<-matrix(c(1/2,-1/4,-1,3/4),nrow=2,ncol=2)
+## Execute: aMatrix<-cachematrix(m1)
+## Execute: aMatrix$get() - will return m1
+## Execute: aMatrix$getinv() - will return NULL
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -16,6 +21,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
+## The cacheSolve will either solve for the inv of the matrix if m is NULLL
+## or retrieve the inv from the object passed in as the argument
+## Execute: cacheSolve(aMatrix) - will return the inv of m1
+## Execute: cacheSolve(aMatrix) - will return the inv of m1 from
+## the data already calculated and print out that it is getting the cached data
+## Execute: aMatrix$getinv() - will return the inv of m1 already
 
 cacheSolve <- function(x, ...) {
         
